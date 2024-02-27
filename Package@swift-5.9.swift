533be7ed32aa9da98da2e1134aee28c6ce3fe9b9
Iurii Khvorost <iurii.khvorost@gmail.com>
2024-02-27 22:39:26 +0200

    Added package@swift-5.9 with visionOS

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -9,6 +9,7 @@ let package = Package(
     .iOS(.v12),
     .macOS(.v10_14),
     .tvOS(.v12),
+    .visionOS(.v1),
     .watchOS(.v5)
   ],
   products: [
@@ -31,3 +32,4 @@ let package = Package(
   ],
   swiftLanguageVersions: [.v5]
 )
+
