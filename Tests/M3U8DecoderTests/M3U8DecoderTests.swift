3548e64395c9324aaf4dd336b70e98a4e441ee98
Iurii Khvorost <iurii.khvorost@gmail.com>
2024-07-23 09:21:55 +0300

    Updated tests

--- Tests/M3U8DecoderTests/M3U8DecoderTests.swift
+++ Tests/M3U8DecoderTests/M3U8DecoderTests.swift
@@ -721,7 +721,7 @@ final class M3U8Tests_File: XCTestCase {
     let playlist = try M3U8Decoder().decode(VideoPlaylist.self, from: data)
     let timeout = -start.timeIntervalSinceNow
     print("Timeout: \(timeout)")
-    XCTAssert(timeout < 5.0)
+    XCTAssert(timeout < 7.0)
   
     XCTAssert(playlist.segments.count == 194918)
     XCTAssert(playlist.segments[1].extinf.duration == -1)
